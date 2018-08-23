28248a6d7fc3d1d64a1dcf73c4f066fbacf06a31
Austin Hai Vuong <avuong>
2018-08-23 10:38:26 -0500

    Austin Major Computer Science

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Austin Vuong and Dan Navarro
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Austin Major Computer Science\n");
+
+  return 0;
+}
